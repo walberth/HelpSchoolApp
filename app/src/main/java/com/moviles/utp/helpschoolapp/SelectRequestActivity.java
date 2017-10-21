73c30906f9c0773d7c463d093eeb1f35c06adc1d
axstavo08 <tavo15g@gmail.com>
2017-10-21 08:00:05 -0500

    two buttons

--- app/src/main/java/com/moviles/utp/helpschoolapp/SelectRequestActivity.java
+++ app/src/main/java/com/moviles/utp/helpschoolapp/SelectRequestActivity.java
@@ -5,23 +5,45 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class SelectRequestActivity extends Activity implements View.OnClickListener {
 
+    EditText text;
     Button send;
+    Button querys;
+    Button responses;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_request);
-        send =  findViewById(R.id.btnSend);
+        text = findViewById(R.id.editText);
+        send = findViewById(R.id.btnSend);
         send.setOnClickListener(this);
+        querys = findViewById(R.id.btnQuerys);
+        querys.setOnClickListener(this);
+        responses = findViewById(R.id.btnResponse);
+        responses.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-        Intent intent = new Intent(this,ListRequestActivity.class);
+
+        if (view == send) {
+            text.setText("");
+            text.requestFocus();
+        } else if (view == querys) {
+            Intent intent = new Intent(this, ListRequestActivity.class);
+            startActivity(intent);
+        } else if (view == responses) {
+            Intent intent = new Intent(this, ResponseActivity.class);
+            startActivity(intent);
+        }
+
+
         //intent.putExtra("name", txtName.getText().toString());
-        startActivity(intent);
+
     }
 }

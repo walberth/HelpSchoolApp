3e6f7a4491b8b84ae05977f551be928ee0ab15eb
axstavo08 <tavo15g@gmail.com>
2017-10-21 04:36:04 -0500

    ignore modules xml

--- app/src/main/java/com/moviles/utp/helpschoolapp/MainActivity.java
+++ app/src/main/java/com/moviles/utp/helpschoolapp/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_reply_request);
+        setContentView(R.layout.activity_main);
     }
 }

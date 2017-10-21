1078493387a5e22f111835d817aaed3684fcc5ba
Walberth Gutierrez Telles <w.felipe.gutierrez@gmail.com>
2017-10-21 03:46:05 -0500

    Adicion de vistas

---
+++ app/src/main/java/com/moviles/utp/helpschoolapp/ListRequestActivity.java
@@ -0,0 +1,14 @@
+package com.moviles.utp.helpschoolapp;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class ListRequestActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_request);
+    }
+
+}

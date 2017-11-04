3ccb99fce16cb6190d38b57c18f3daae0ab27093
JuanHuertaB <juancarloshuertabarzola@gmail.com>
2017-11-04 07:10:52 -0500

    Fragments conectados

--- app/src/main/java/com/moviles/utp/helpschoolapp/ContainerActivity.java
+++ app/src/main/java/com/moviles/utp/helpschoolapp/ContainerActivity.java
@@ -1,9 +1,17 @@
 package com.moviles.utp.helpschoolapp;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+
+import com.moviles.utp.helpschoolapp.ui.fragment.AccountFragment;
+import com.moviles.utp.helpschoolapp.ui.fragment.DoRequestFragment;
+import com.moviles.utp.helpschoolapp.ui.fragment.HistoricalFragment;
+import com.moviles.utp.helpschoolapp.ui.fragment.ListRequestFragment;
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabSelectListener;
+
+/////////import com.example.juanc.platziandroid.View.fragment.HomeFragment;
 
 public class ContainerActivity extends AppCompatActivity {
 
@@ -11,9 +19,40 @@ public class ContainerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_container);
-    }
-    public void onClick(View view){
-        Intent intent =  new Intent(this,ContainerActivity.class);
-        startActivity(intent);
+        BottomBar bottomBar = (BottomBar) findViewById(R.id.bottombar);
+       // bottomBar.setDefaultTab(R.id.account);
+
+        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+
+            @Override
+            public void onTabSelected(int tabId) {
+                switch (tabId){
+                    case R.id.account:
+                        AccountFragment accountFragment= new AccountFragment();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, accountFragment)
+                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                                .addToBackStack(null).commit();
+                        break;
+                    case R.id.do_request:
+                        DoRequestFragment doRequestFragment= new DoRequestFragment();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, doRequestFragment )
+                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                                .addToBackStack(null).commit();
+                        break;
+                    case R.id.list_request:
+                        ListRequestFragment listRequestFragment= new ListRequestFragment();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, listRequestFragment)
+                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                                .addToBackStack(null).commit();
+                        break;
+                    case R.id.historical:
+                        HistoricalFragment historicalFragment = new HistoricalFragment();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, historicalFragment)
+                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                                .addToBackStack(null).commit();
+                        break;
+                }
+            }
+        });
     }
 }

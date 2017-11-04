06d7c7f4b8cd8254439fb046022f3e426f64c906
axstavo08 <tavo15g@gmail.com>
2017-11-04 08:23:52 -0500

    init response login

--- app/src/main/java/com/moviles/utp/helpschoolapp/ContainerActivity.java
+++ app/src/main/java/com/moviles/utp/helpschoolapp/ContainerActivity.java
@@ -5,12 +5,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.moviles.utp.helpschoolapp.data.storage.UserSessionManager;
+
 public class ContainerActivity extends AppCompatActivity {
+
+    private UserSessionManager session;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_container);
+
     }
     public void onClick(View view){
         Intent intent =  new Intent(this,ContainerActivity.class);

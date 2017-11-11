806a01d49647515831da832871e405873c7e1301
axstavo08 <tavo15g@gmail.com>
2017-11-11 01:03:36 -0500

    finish login

--- app/src/main/java/com/moviles/utp/helpschoolapp/LoginActivity.java
+++ app/src/main/java/com/moviles/utp/helpschoolapp/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.moviles.utp.helpschoolapp;
 
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.AsyncTask;
@@ -9,6 +10,7 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -54,6 +56,8 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     @Override
     public void onClick(View v) {
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
         boolean isOk = true;
         TextInputLayout component = null;
         //Valida que campos usuario y clave tengan texto
--- app/src/main/java/com/moviles/utp/helpschoolapp/ui/fragment/AccountFragment.java
+++ app/src/main/java/com/moviles/utp/helpschoolapp/ui/fragment/AccountFragment.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.moviles.utp.helpschoolapp.R;
+import com.moviles.utp.helpschoolapp.data.storage.UserSessionManager;
 
 
 /**
@@ -15,6 +16,7 @@ import com.moviles.utp.helpschoolapp.R;
  */
 public class AccountFragment extends Fragment {
 
+    private UserSessionManager session;
 
     public AccountFragment() {
         // Required empty public constructor
@@ -24,6 +26,15 @@ public class AccountFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        /*session = new UserSessionManager(getApplicationContext());
+        if (session.checkLogin()) finish();
+        Map<String, String> user = session.getUserDetails();
+        new UserResponse(user.get(UserSessionManager.KEY_USERNAME),
+                user.get(UserSessionManager.KEY_NAME),
+                user.get(UserSessionManager.KEY_FATHERLASTNAME),
+                user.get(UserSessionManager.KEY_MOTHERLASTNAME),
+                user.get(UserSessionManager.KEY_EMAIL),
+                user.get(UserSessionManager.KEY_PROFILE));*/
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_account, container, false);
     }

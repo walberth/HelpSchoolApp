d6b5594810c6d49a21ec86d1e48e96baada80a4e
JuanHuertaB <juancarloshuertabarzola@gmail.com>
2017-11-14 21:57:40 -0500

    Envio de solicitud

--- app/src/main/java/com/moviles/utp/helpschoolapp/ui/fragment/DoRequestFragment.java
+++ app/src/main/java/com/moviles/utp/helpschoolapp/ui/fragment/DoRequestFragment.java
@@ -23,6 +23,7 @@ import com.moviles.utp.helpschoolapp.data.model.PendingRequestResponse;
 import com.moviles.utp.helpschoolapp.ui.adapter.ListRequestAdapterRecyclerView;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
@@ -31,6 +32,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.ArrayList;
@@ -47,7 +49,7 @@ import java.util.Map;
 public class DoRequestFragment extends Fragment {
     private static final String TAG = "DoRequestActivity";
     private static final String URL_WS = "http://wshelpdeskutp.azurewebsites.net/listRequestType/";
-    private static final String URL_WS1 = "http://wshelpdeskutp.azurewebsites.net/createRequestByAppl/";
+    private static final String URL_WS2 = "http://wshelpdeskutp.azurewebsites.net/createRequestByAppl/";
     private String username = "GUSTAVO.RAMOS";
     private String type = "0";
     private Button sendRequest;
@@ -72,6 +74,7 @@ public class DoRequestFragment extends Fragment {
         private String content;
         private View view;
         private ListRequestType listRequestType;
+
         private ArrayList<ListRequestType> listRequestTypeList = new ArrayList<>();
 
         protected void onPreExecute() {
@@ -211,6 +214,120 @@ public class DoRequestFragment extends Fragment {
             Log.d(TAG, "convertJSONObjectToStringParams: ends");
             return stringBuilder.toString();
         }
+
+    }
+
+    //ENVIO DE SOLICITUD
+
+    public class SendRequest extends AsyncTask<Object,Void,Void>{
+        private ProgressDialog dialogo = new ProgressDialog(getActivity());
+        private String content;
+        private View view;
+        private SendRequest confirmation;
+        private ArrayList<SendRequest> confirmationList= new ArrayList<>();
+        public SendRequest(String status) {
+
+        }
+
+        protected void onPreExecute(){
+            dialogo.setMessage("Enviando solicitud...");
+            dialogo.show();
+            dialogo.setCancelable(false);
+        }
+
+        @Override
+        protected Void doInBackground(Object... strings) {
+            postData((String) strings[0], (String) strings[1], (String) strings[2]);
+            view = (View) strings[3];
+            return null;
+        }
+
+        protected void onPostExecute(Void aVoid){
+            JSONArray jsonResponse;
+
+            try {
+                jsonResponse = new JSONArray(content);
+
+                for(int i = 0; i<jsonResponse.length();i++){
+                    JSONObject jsonObject = jsonResponse.getJSONObject(i);
+                    confirmation = new SendRequest(
+                        jsonObject.getString("status"));
+                }
+
+                confirmationList.add(confirmation);
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+
+
+        }
+
+
+
+        private void postData(String name, String idRequest, String description) {
+            URL url = null;
+
+            try {
+                url = new URL(URL_WS2);
+                JSONObject objParams = new JSONObject();
+                objParams.put("nameAppl",name);
+                objParams.put("idRequestType",idRequest);
+                objParams.put("description",description);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setConnectTimeout(15000);
+                connection.setReadTimeout(15000);
+                connection.setRequestMethod("POST");
+                connection.setDoInput(true);
+                connection.setDoOutput(true);
+                OutputStream outputStream = connection.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+                writer.write(convertJSONObjectToStringParams(objParams));
+                writer.flush();
+                writer.close();
+                outputStream.close();
+
+                int responseCode = connection.getResponseCode();
+
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    StringBuffer stringBuffer = new StringBuffer("");
+                    String line;
+
+                    while ((line = bufferedReader.readLine()) != null) {
+                        stringBuffer.append(line);
+                        break;
+                    }
+
+                    bufferedReader.close();
+                    content = stringBuffer.toString();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        private String convertJSONObjectToStringParams(JSONObject params) throws Exception{
+            StringBuilder stringBuilder = new StringBuilder();
+            boolean first = true;
+            Iterator<String> iterator = params.keys();
+
+            while(iterator.hasNext()){
+                String key = iterator.next();
+                Object value = params.get(key);
+                if (first) first = false;
+                else stringBuilder.append("&");
+                stringBuilder.append(URLEncoder.encode(key, "UTF-8"));
+                stringBuilder.append("=");
+                stringBuilder.append(URLEncoder.encode(value.toString(), "UTF-8"));
+            }
+                return stringBuilder.toString();
+        }
+
+
+
 
     }
 }
